305c0db80b1161d753dfe0a64bc21af6cb4aff73
ohnishant <nishant.mishra550@gmail.com>
2023-06-04 09:55:01 +0530

    lexer: add keyword basic code, push to upstream for backup

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,13 +1,19 @@
 #![allow(dead_code)]
 
+use crate::token;
 use crate::token::Token;
 use crate::token::TokenType;
+
 
 #[derive(Debug)]
 pub struct Lexer {
     input: String,
     position: usize,
     current_char: Option<char>,
+}
+
+fn is_letter(ch: char) -> bool {
+    return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_';
 }
 
 impl Lexer {
@@ -27,12 +33,14 @@ impl Lexer {
 
     fn advance(&mut self) {
         self.position += 1;
+        self.get_current_char();
     }
 
     fn is_whitespace(&self, ch: char) -> bool {
         ch.is_whitespace() || ch == '\n' || ch == '\r'
     }
 
+ 
     fn skip_whitespace(&mut self) {
         while let Some(ch) = self.get_current_char() {
             if !self.is_whitespace(ch) {
@@ -47,8 +55,29 @@ impl Lexer {
         self.input.chars().nth(self.position + 1)
     }
     
+    pub fn read_identifier(&mut self) -> String {
+        let mut idntifier = String::new();
+
+        while let Some(ch) = self.get_current_char() {
+            if !ch.is_ascii_alphanumeric() && ch != '_' {
+                break;
+            }
+            idntifier.push(ch);
+            self.advance();
+        }
+        return idntifier;
+    }
+
+    fn lookup_ident(&mut self,  tok: &mut token::Token) -> token::TokenType {
+        match tok.literal.as_str() {
+            "fn" => return TokenType::FUNCTION,
+            "let" => return TokenType::LET,
+            _ => return TokenType::IDENT("thing".to_string()),
+        }
+    }
+
     pub fn next_token(&mut self) -> Token {
-        let tok: Token;
+        let mut tok: Token;
 
         if let Some(ch) = self.get_current_char() {
             match ch {
@@ -61,7 +90,16 @@ impl Lexer {
                 '{' => tok = Token::new_token(TokenType::LBRACE, ch.to_string()),
                 '}' => tok = Token::new_token(TokenType::RBRACE, ch.to_string()),
                 // '\0' => tok = Token::new_token(TokenType::EOF,"".to_string()),
-                _ => tok = Token::new_token(TokenType::ILLEGAL, ch.to_string()),
+                _ => {
+                    if is_letter(ch) {
+                        let lit = self.read_identifier();
+                        let tok_kind = self.lookup_ident(&mut tok);
+                        tok = Token::new_token(tok_kind, lit)
+                    }
+                    else {
+                        tok = Token::new_token(TokenType::ILLEGAL, ch.to_string())
+                    }
+                },
             }
         }
         else {
