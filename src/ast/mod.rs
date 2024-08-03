492852cb1a3e325b9bf1e8b9eeb0d98fb4a95f73
OhNishant <nishant.mishra550@gmail.com>
2024-08-03 19:02:13 +0530

    add: expressions also implement the node interface on all the node structs pepeW TODO: deal with the string lifetimes here

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -1,13 +1,17 @@
 use crate::token::Token;
 
 pub trait Node {
-    fn token_literal(&self) -> &String;
+    fn token_literal(&self) -> &String {
+        todo!()
+    }
+    fn to_string(&self) -> &String;
 }
 
 #[derive(Debug)]
 pub enum Statement {
     Let(LetStatement),
     Return(ReturnStatement),
+    Expression(ExpressionStatement),
 }
 
 #[derive(Debug)]
@@ -21,6 +25,27 @@ pub struct Program {
     pub statements: Vec<Statement>,
 }
 
+impl Node for Program {
+    // TODO: Implement this. deal with the string lifetimes lol
+    fn token_literal(&self) -> &String {
+        if self.statements.len() > 0 {
+            let statement = &self.statements[0];
+            match statement {
+                Statement::Let(_) => todo!(),
+                Statement::Return(_) => todo!(),
+                Statement::Expression(_) => todo!(),
+                _ => unimplemented!("[ERROR]: Unimplemented statement type"),
+            }
+        } else {
+            todo!();
+        }
+    }
+
+    fn to_string(&self) -> &String {
+        todo!()
+    }
+}
+
 #[derive(Debug)]
 pub struct LetStatement {
     pub token: Token,
@@ -28,10 +53,56 @@ pub struct LetStatement {
     pub value: Expression,
 }
 
+impl Node for LetStatement {
+    fn token_literal(&self) -> &String {
+        return &self.token.literal;
+    }
+
+    fn to_string(&self) -> &String {
+        // FIXME: Implement this
+        match &self.value {
+            Expression::Identifier(ident) => return &ident.name,
+            Expression::Literal(lit) => return &lit.value,
+        };
+    }
+}
+
 #[derive(Debug)]
 pub struct ReturnStatement {
     pub token: Token,
     pub return_value: Expression,
+}
+
+impl Node for ReturnStatement {
+    fn token_literal(&self) -> &String {
+        return &self.token.literal;
+    }
+
+    fn to_string(&self) -> &String {
+        match &self.return_value {
+            Expression::Identifier(ident) => return &ident.name,
+            Expression::Literal(lit) => return &lit.value,
+        };
+    }
+}
+
+#[derive(Debug)]
+pub struct ExpressionStatement {
+    pub token: Token,
+    pub expression: Expression,
+}
+
+impl Node for ExpressionStatement {
+    fn token_literal(&self) -> &String {
+        return &self.token.literal;
+    }
+
+    fn to_string(&self) -> &String {
+        match &self.expression {
+            Expression::Identifier(ident) => return &ident.name,
+            Expression::Literal(lit) => return &lit.value,
+        };
+    }
 }
 
 #[derive(Debug)]
